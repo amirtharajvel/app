72317a3c42fb935d6867a0fda5877725a8e72d7f
amirtharajvel <132637695+amirtharajvel@users.noreply.github.com>
2024-02-07 15:33:01 +0530

    Docker image running in background
    docker build . -t nodeapp
    docker run -d -it -p 8080:8080 nodeapp

--- Dockerfile
+++ Dockerfile
@@ -1,7 +1,14 @@
-FROM node:21-alpine3.18
+FROM node:18-alpine
+
+WORKDIR /app
+COPY package*.json ./
+
 RUN npm install
+
+COPY . .
+
 EXPOSE 8080
-CMD ["npm"] ["start"]
+CMD ["node"] ["app.js"] ["--production"]
 
 
 
--- app.js
+++ app.js
@@ -1,9 +1,13 @@
-let express = require('express')
+const express = require('express')
+
 let app = express()
 
 app.get('/', (req, res) => {
   res.send('<h1>Hello World!</h1>')
 })
-app.listen(8080, () => {
-  console.log('Simple web app running on port 8080')
+app.listen(8080, '0.0.0.0')
+
+process.on('uncaughtException', (e) => {
+  console.error(e) // try console.log if that doesn't work
+  process.exit(10)
 })
